58af5cf9484f53b746becd4ef8b1da3ddb625e26
Lewin <lewinkoon+github@gmail.com>
2024-03-01 20:55:05 +0100

    remove `home-manager`

--- flake.nix
+++ flake.nix
@@ -3,23 +3,14 @@
 
   inputs = {
     # nixOS official package source
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
-    
-    # home manager
-    home-manager.url = "github:nix-community/home-manager/release-23.11";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+  outputs = { self, nixpkgs, ... }@inputs: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
-        home-manager.nixosModules.home-manager {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.lewin = import ./home.nix;
-        }
       ];
     };
   };
